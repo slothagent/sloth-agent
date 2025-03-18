4fb7ffabcbb2cea75ebf29e2e1dc54bacde78f5a
louisdevzz <vohuunhan1310@gmail.com>
2025-03-19 05:18:31 +0700

    fix: ws

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -12,7 +12,7 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   const { tokens, loading } = useSolanaTokens();
-  console.log(tokens[0],loading);
+  // console.log(tokens[0],loading);
   return (
     <main className="min-h-screen mx-auto flex flex-col bg-[#0B0E17]">   
       <div className="w-full">
